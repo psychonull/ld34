5f82c49ea6bff16b4f464d40adc354c4736dc892
Matias Arriola <matias.arriola@gmail.com>
2015-12-12 22:54:38 -0300

    menu and menuItem basic implementation

--- game/states/menu.js
+++ game/states/menu.js
@@ -1,18 +1,46 @@
 
 'use strict';
-import _ from 'lodash';
 
-export default class Menu {
+import _ from 'lodash';
+import Menu from '../ui/menu';
+
+export default class MenuState {
   preload() {
 
   }
 
   create() {
     this.titleText = this.game.add.bitmapText(400,100, 'p2', 'Menu!', 42);
-    this.titleText = this.game.add.bitmapText(400,300, 'pixelade', 'press A + B to continue', 42);
-    this.titleText.anchor.setTo(0.5, 0.5);
+    // this.titleText = this.game.add.bitmapText(400,300, 'pixelade', 'press A + B to continue', 42);
+    // this.titleText.anchor.setTo(0.5, 0.5);
 
-    this.game.i.AB.onDown.add(this.passToNextState, this);
+    this.menu = new Menu(this.game, {
+      options: [
+        {
+          id: 'start',
+          text: 'START GAME',
+          onSelected: () => {
+            this.game.state.start('play');
+          }
+        },
+        {
+          id: 'instructions',
+          text: 'INSTRUCTIONS',
+          onSelected: () => {
+            this.game.state.start('instructions');
+          }
+        }
+      ],
+      signals: {
+        next: this.game.i.B.onDown,
+        prev: this.game.i.A.onDown,
+        select: this.game.i.AB.onDown
+      }
+    });
+
+    this.game.add.existing(this.menu);
+
+    //this.game.i.AB.onDown.add(this.passToNextState, this);
   }
 
   update () {
---
+++ game/ui/menu.js
@@ -0,0 +1,123 @@
+'use strict';
+
+import MenuItem from './menuItem';
+import _ from 'lodash';
+
+const defaultOptions = {
+  menuItemClass: MenuItem,
+  options: [], // [{ id: 'start', text: 'START NOW', onSelected: function(){}}]
+  itemHeight: 50,
+  signals: {}
+};
+
+export default class Menu extends Phaser.Group {
+
+  constructor(game, config) {
+    super(game);
+    this._config = _.merge({}, defaultOptions, config);
+    this.menuItems = [];
+    this.setup();
+  }
+
+  setup(){
+    this.fixedToCamera = true;
+    var currentOptionIndex = 0;
+    for(let option of this._config.options){
+      let menuOption = _.merge({}, option, {
+        style: {
+          y: currentOptionIndex * this._config.itemHeight
+        }
+      });
+      let menuItem = new this._config.menuItemClass(this.game, menuOption);
+      this.menuItems.push(menuItem);
+      //this.game.add.existing(menuItem);
+      this.add(menuItem);
+      // something else?
+      currentOptionIndex++;
+    }
+
+    if(this.menuItems.length && !_.any(this.menuItems, (mi) => mi.highlighted)){
+      this.menuItems[0].highlight();
+    }
+
+    this.setupSignals();
+  }
+
+  setupSignals(){
+    var signals = {
+      next: new Phaser.Signal(),
+      prev: new Phaser.Signal(),
+      select: new Phaser.Signal()
+    };
+    const signalMap = {
+      next: this.highlightNext,
+      prev: this.highlightPrev,
+      select: this.select
+    };
+    if(!this._config.signals.select){
+      window.console.warn('No select signal passed to menu');
+    }
+    for (let signal of Object.keys(this._config.signals)){
+      if(signalMap[signal]){
+        this._config.signals[signal].add(signalMap[signal], this);
+      }
+      else {
+        window.console.warn('Unknown menu signal: ' + signal);
+      }
+    }
+
+  }
+
+  highlight(index){
+    this.unhighlightCurrent();
+    if(this.menuItems[index]){
+      this.menuItems[index].highlight();
+    }
+  }
+
+  highlightNext(){
+    var index = this.getHighlightedIndex();
+    this.unhighlightCurrent();
+    if(this.menuItems[index + 1]){
+      this.menuItems[index + 1].highlight();
+    }
+    else {
+      this.menuItems[0].highlight();
+    }
+  }
+
+  highlightPrev(){
+    var index = this.getHighlightedIndex();
+    this.unhighlightCurrent();
+    if(index === 0){
+      this.menuItems[this.menuItems.length - 1].highlight();
+    }
+    else {
+      this.menuItems[index - 1].highlight();
+    }
+  }
+
+  unhighlightCurrent() {
+    var currentlyHighlighted = _.find(this.menuItems, (mi) => mi.highlighted);
+    if(currentlyHighlighted){
+      currentlyHighlighted.unhighlight();
+    }
+  }
+
+  getHighlightedIndex(){
+    var currentlyHighlightedIndex = _.findIndex(this.menuItems, (mi) => mi.highlighted);
+    if (currentlyHighlightedIndex === -1){ //defensive programming (?)
+      return 0;
+    }
+    return currentlyHighlightedIndex;
+  }
+
+  select(){
+    var currentlyHighlighted = _.find(this.menuItems, (mi) => mi.highlighted);
+    if(currentlyHighlighted){
+      currentlyHighlighted.select();
+    }
+  }
+
+
+};
