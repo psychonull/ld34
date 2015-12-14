9f11a4e2c1fd9b4f19c7f23aa70f8c22408a6874
Matias Arriola <matias.arriola@gmail.com>
2015-12-14 02:02:09 -0300

    menu small improvement

--- game/states/menu.js
+++ game/states/menu.js
@@ -3,6 +3,7 @@
 
 import _ from 'lodash';
 import Menu from '../ui/menu';
+import Popup from '../prefabs/ui/genericPopup';
 
 export default class MenuState {
   preload() {
@@ -10,16 +11,19 @@ export default class MenuState {
   }
 
   create() {
-    this.titleText = this.game.add.bitmapText(400,100, 'p2', 'Menu!', 42);
+    this.titleText = this.game.add.bitmapText(this.game.world.centerX, 200, 'p2', 'Nombre del juego', 38);
+    this.titleText.anchor.setTo(0.5, 0.5);
     this.eKey = this.game.input.keyboard.addKey(Phaser.Keyboard.E);
     // this.titleText = this.game.add.bitmapText(400,300, 'pixelade', 'press A + B to continue', 42);
-    // this.titleText.anchor.setTo(0.5, 0.5);
+    // this.titleText.anchor.setTo(0.5, 0.5)
 
-    this.menu = new Menu(this.game, {
+    var menu = new Menu(this.game, {
+      x: 10,
+      y: 10,
       options: [
         {
           id: 'start',
-          text: 'START GAME',
+          text: 'NEW GAME',
           onSelected: () => {
             this.game.state.start('play');
           }
@@ -39,7 +43,13 @@ export default class MenuState {
       }
     });
 
-    this.game.add.existing(this.menu);
+    this.menuFrame = new Popup(this.game, {
+      x:300,
+      y:350,
+      content: menu
+    });
+
+    this.game.add.existing(this.menuFrame);
 
     //this.game.i.AB.onDown.add(this.passToNextState, this);
   }
