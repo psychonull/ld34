7f5eef2711c63b35dd349342464e99944b1dc07b
Matias Arriola <matias.arriola@gmail.com>
2015-12-14 18:25:46 -0300

    menu + help

--- game/states/instructions.js
+++ game/states/instructions.js
@@ -1,6 +1,8 @@
 
 'use strict';
 
+import Popup from '../prefabs/ui/genericPopup';
+
 export default class Instructions {
   preload() {
 
@@ -10,8 +12,19 @@ export default class Instructions {
     this.titleText = this.game.add.bitmapText(400, 100, 'p2', 'Instructions!', 42);
     this.titleText.anchor.setTo(0.5, 0.5);
 
-    this.titleText = this.game.add.bitmapText(200, 400, 'pixelade', 'Press A to go back', 24);
-    this.titleText.anchor.setTo(0.5, 0.5);
+    this.helpText = this.game.add.bitmapText(200, 570, 'pixelade', '<< Z to go back >>', 20);
+    this.helpText.anchor.setTo(0.5, 0.5);
+
+    this.instructions = new Popup(this.game, {
+      content: `* Z to shoot (hold to charge power)
+* X to call other player
+* Don't lose the ball.
+* Score to win`,
+      x: 150,
+      y: 200,
+      width: 500
+    });
+    this.game.add.existing(this.instructions);
 
     this.game.i.A.onDown.addOnce(() => this.game.state.start('menu'));
   }
