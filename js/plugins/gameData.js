dcb24a9ce32fc77dac5bfb5882be0a3022193ba6
Matias Arriola <matias.arriola@gmail.com>
2015-12-14 22:02:24 -0300

    Built ld34 from commit 0cc0377 on branch master

--- js/plugins/gameData.js
+++ js/plugins/gameData.js
@@ -15,11 +15,18 @@ const baseData = {
   roster: [],
   team: [], // titulares
   founder: null,
-  currentLevel: 0
+  currentLevel: 0,
+  nextLevel: 0
 };
 
 const baseInternals = {
   firstTimePlayed: true
+};
+
+const baseState = {
+  hasPlayerToClaim: false,
+  recentlyClaimed: [],
+  recentlyLost: false
 };
 
 export default class GameData extends Phaser.Plugin {
@@ -32,6 +39,7 @@ export default class GameData extends Phaser.Plugin {
   init(){
     this._data = baseData;
     this._internals = this._loadInternals();
+    this._state = baseState;
     this.settings = config;
     this.onChange = new Phaser.Signal();
     this.onLoad = new Phaser.Signal();
@@ -46,6 +54,10 @@ export default class GameData extends Phaser.Plugin {
     let oldValue = this._data[key];
     this._data[key] = value;
     this.onChange.dispatch(key, value, oldValue);
+  }
+
+  inc(key){
+    this.set(key, this._data[key] + 1);
   }
 
   save(){
@@ -109,6 +121,14 @@ export default class GameData extends Phaser.Plugin {
     this.set('founder', player);
     this.set('roster', [player]);
     this.set('team', [player]);
+  }
+
+  selectNewMember(player){
+    var roster = this.get('roster');
+    roster.push(player);
+    this.set('roster', roster);
+    // roster === team because of time :(
+    this.set('team', roster);
   }
 
   destroy(){
